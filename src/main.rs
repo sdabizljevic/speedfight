4de5c91e5c36d1f6f845418db6dbac4983c3d38b
sdabizljevic <stevand2@illinois.edu>
2023-04-19 23:47:13 -0500

    Added basic fight capability, still only one stage. Added turn counter. Added win or loss screen.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,15 @@ use quicksilver::{
     input::{Key, MouseButton},
     run, Graphics, Input, Result, Settings, Timer, Window,
 };
+use rand::Rng;
+use std::{thread, time};
+
+//struct for any entity in the game, including the hero and enemies.
+struct Entity {
+    max_health: f32,
+    current_health: f32,
+    moves_unlocked: u8,
+}
 
 fn main() {
     run(
@@ -20,9 +29,9 @@ fn main() {
 async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
     //loads all necessary images and fonts.
     let castle_image = Image::load(&gfx, "titlescreen_background.png").await?;
-    let hero = Image::load(&gfx, "fancy_pants.png").await?;
-    let arena1 = Image::load(&gfx, "sewer_background.png").await?;
-    let enemy1 = Image::load(&gfx, "bokoblin.png").await?;
+    let hero_image = Image::load(&gfx, "fancy_pants.png").await?;
+    let arena1_image = Image::load(&gfx, "sewer_background.png").await?;
+    let enemy1_image = Image::load(&gfx, "bokoblin.png").await?;
     let title_font = VectorFont::load("title_font.ttf").await?;
     let main_font = VectorFont::load("main_font.ttf").await?;
     let punch_image = Image::load(&gfx, "punch.png").await?;
@@ -30,15 +39,28 @@ async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> 
 
     //creates necessary variables, like the frame counter or title screen tracker.
     let mut frames = Timer::time_per_second(30.0);
-    let mut on_title_screen: u8 = 0;
+    let mut screen_tracker: u8 = 0;
+    let mut turn: f32 = 0.0;
+
+    //creats hero and enemy in advance.
+    let mut hero = Entity {
+        max_health: 100.0,
+        current_health: 100.0,
+        moves_unlocked: 2,
+    };
+    let mut enemy = Entity {
+        max_health: 100.0,
+        current_health: 100.0,
+        moves_unlocked: 2,
+    };
 
     //starts running the game, this allows me to exit the game on command (whenever someone presses ESC).
     let mut running = true;
     while running {
         //loads title screen only on open.
-        if on_title_screen == 0 {
+        if screen_tracker == 0 {
             title_screen(&window, &mut gfx, &castle_image, &title_font)?;
-            on_title_screen = 1;
+            screen_tracker = 1;
         }
 
         //constantly loops checking for inputs.
@@ -47,7 +69,7 @@ async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> 
             if input.key_down(Key::Escape) { running = false; }
 
             //if on the title screen, space will start the game.
-            if input.key_down(Key::Space) && on_title_screen == 1 { 
+            if input.key_down(Key::Space) && screen_tracker == 1 { 
                 //fades the screen to black.
                 let mut alpha: f32 = 0.0;
                 let rect = Rectangle::new(Vector::new(0.0, 0.0), Vector::new(1100.0, 800.0));
@@ -58,17 +80,55 @@ async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> 
                         gfx.present(&window)?;
                     }
                 }
-                gfx.clear(Color::BLACK);
-                gfx.present(&window)?;
-                on_title_screen = 2;
+                screen_tracker = 2;
 
-                //loads the first fight
-                load_fight(&window, &mut gfx, &arena1, &hero, &enemy1);
-                fight_one(&window, &mut gfx, &main_font, &punch_image, &kick_image);
+                //loads the visuals for the first fight.
+                load_fight(&mut gfx, &arena1_image, &hero_image, &enemy1_image);
+                fight_one(&mut gfx, &main_font, &punch_image, &kick_image, );
+
+                //loads the initial turn counter and health bars.
+                health_bars_update(&mut gfx, &hero, &enemy);
+                gfx.present(&window)?;
             }
 
+            //does the proper calculations and display in response to using a "punch". Also checks for a win after the punch lands.
+            if input.key_down(Key::Q) && hero.moves_unlocked >= 1 && screen_tracker < 90 { 
+                //punches do 18 damage, also immediately increment the turn counter.
+                let punch_damage: f32 = 9.0;
+                turn += 0.5;
+
+                //calls on my function to calculate an enemies move.
+                let enemy_damage = enemy_move(&enemy);
+                
+                if enemy_damage == -1.0 {
+                    //do nothing, attack was blocked.
+                } else {
+                    enemy.current_health = enemy.current_health - punch_damage;
+                    hero.current_health = hero.current_health - enemy_damage;
+                }
+
+                //updates the health bar visuals.
+                health_bars_update(&mut gfx, &hero, &enemy);
+                gfx.present(&window)?;
+
+                //checks for win or loss.
+                if enemy.current_health <= 0.0 { //win condition.
+                    screen_tracker = 99;
+
+                    gfx.clear(Color::BLACK);
+                    text_box(&mut gfx, 1, &main_font, turn);
+                    gfx.present(&window)?;
+                } else if hero.current_health <= 0.0 { //loss condition.
+                    screen_tracker = 99;
+                    
+                    gfx.clear(Color::BLACK);
+                    text_box(&mut gfx, 2, &main_font, turn);
+                    gfx.present(&window)?;
+                }
+            }
         }
     }
+
     Ok(())
 }
 
@@ -123,7 +183,7 @@ fn title_screen(window: &Window, mut gfx: &mut Graphics, castle_image: &Image, t
 }
 
 //loads the general fightscene, aside from health bars and other varying graphics that will change frequently.
-fn load_fight(window: &Window, mut gfx: &mut Graphics, background_image: &Image, hero_image: &Image, enemy_image: &Image) -> Result<()> {
+fn load_fight(mut gfx: &mut Graphics, background_image: &Image, hero_image: &Image, enemy_image: &Image) -> Result<()> {
     gfx.clear(Color::BLACK);
 
     //draws background with hero and enemy on top.
@@ -138,12 +198,12 @@ fn load_fight(window: &Window, mut gfx: &mut Graphics, background_image: &Image,
 }
 
 //function to update health bar rectangles.
-fn health_bars_update(window: &Window, mut gfx: &mut Graphics, hero_health: &f32, enemy_health: &f32, enemy_total_health: &f32) -> Result<()> {
+fn health_bars_update(mut gfx: &mut Graphics, hero: &Entity, enemy: &Entity) -> Result<()> {
     //create the widths for the red and green portion of the health bars.
-    let hero_green_width: f32 = (hero_health).clone();
-    let hero_red_width: f32 = (100.0 - hero_health).clone();
-    let enemy_green_width: f32 = (enemy_health).clone();
-    let enemy_red_width: f32 = (enemy_total_health - enemy_health).clone();
+    let hero_green_width: f32 = (hero.current_health).clone();
+    let hero_red_width: f32 = (hero.max_health - hero.current_health).clone();
+    let enemy_green_width: f32 = (enemy.current_health).clone();
+    let enemy_red_width: f32 = (enemy.max_health - enemy.current_health).clone();
     
     //creates and fills in the rectangles.
     let hero_green_rectangle = Rectangle::new(Vector::new(100.0, 350.0), Vector::new(hero_green_width * 2.0, 25.0));
@@ -158,22 +218,52 @@ fn health_bars_update(window: &Window, mut gfx: &mut Graphics, hero_health: &f32
     Ok(())
 }
 
-//function to update the turn count.
-fn turn_counter_update(window: &Window, mut gfx: &mut Graphics, font: &VectorFont, turn_count: &i32) -> Result<()> {
-    let turn_count_string = turn_count.to_string();
-    let turn_label = font.to_renderer(&gfx, 50.0);
-    turn_label?.draw(
-        &mut gfx, 
-        &format!("Turn {}", turn_count_string).as_str(), 
-        Color::GREEN, 
-        Vector::new(450.0, 50.0)
-    )?;
+//decides enemy move.
+fn enemy_move(enemy: &Entity) -> f32 {
+    //creates a random number
+    let random = rand::thread_rng().gen_range(0..9);
+    
+    //if statements use different logic based on which enemy you're facing.
+    if enemy.moves_unlocked == 2 {
+        if random >= 0 && random <= 4 { //50 percent change to punch.
+            return 8.0;
+        } else if random >= 5 && random <= 7 { //30 percent chance to block.
+            return -1.0;
+        } else { //20 percent chance to stumble.
+            return 0.0;
+        }
+    } else { return 0.0; }
+}
+
+//function to put up the text box, which clears after the player interacts.
+fn text_box(mut gfx: &mut Graphics, display: u8, font: &VectorFont, turn: f32) -> Result<()> {
+    let black_box = Rectangle::new(Vector::new(150.0, 500.0), Vector::new(700.0, 200.0));
+    gfx.fill_rect(&black_box, Color::BLACK);
+    gfx.stroke_rect(&black_box, Color::WHITE);
+
+    if display == 1 { //text box for when you win.
+        let win_text = font.to_renderer(&gfx, 40.0);
+        win_text?.draw(
+            &mut gfx, 
+            format!("You've won! In {} turns!", turn).as_str(), 
+            Color::WHITE, 
+            Vector::new(165.0, 550.0)
+        )?;
+    } else if display == 2 { //text box for when you lose.
+        let lose_text = font.to_renderer(&gfx, 30.0);
+        lose_text?.draw(
+            &mut gfx, 
+            format!("You've lost... At least you lasted {} turns!", turn).as_str(), 
+            Color::WHITE, 
+            Vector::new(165.0, 550.0)
+        )?;
+    }
 
     Ok(())
 }
 
-//function that loads the bulk of the first fight. (fight label, available move icons, turn counter, and health bars)
-fn fight_one(window: &Window, mut gfx: &mut Graphics, font: &VectorFont, punch_image: &Image, kick_image: &Image) -> Result<()> {
+//function that loads images specific to the first fight. (fight label, available move icons)
+fn fight_one(mut gfx: &mut Graphics, font: &VectorFont, punch_image: &Image, kick_image: &Image) -> Result<()> {
     //labels fight 1 at the top.
     let fight_label = font.to_renderer(&gfx, 50.0);
     fight_label?.draw(
@@ -209,12 +299,5 @@ fn fight_one(window: &Window, mut gfx: &mut Graphics, font: &VectorFont, punch_i
         Vector::new(270.0, 220.0)
     )?;
     
-    //loads turn counter.
-    turn_counter_update(&window, &mut gfx, &font, &1);
-
-    //loads health bars.
-    health_bars_update(&window, &mut gfx, &50.0, &50.0, &100.0);
-
-    gfx.present(&window)?;
     Ok(())
 }
